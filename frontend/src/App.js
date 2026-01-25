98a75cfba896f6bfd2443843c16b386068202a80
Gaurav Rajput <156045928+Grvrajput@users.noreply.github.com>
2026-01-25 12:12:28 +0530

    refresh update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+// CHANGE THIS LINE:
+import { HashRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './styles/App.css';
@@ -16,6 +17,7 @@ function App() {
           <div className="container">
             <h1 className="logo">HRMS Lite</h1>
             <div className="nav-links">
+              {/* Links will automatically work with HashRouter */}
               <Link to="/" className="nav-link">Dashboard</Link>
               <Link to="/employees" className="nav-link">Employees</Link>
               <Link to="/attendance" className="nav-link">Attendance</Link>
